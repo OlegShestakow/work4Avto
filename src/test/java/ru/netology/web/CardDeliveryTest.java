8a40d4bfb7807602851aa8cff7b2fd9b84be6c57
Oleg Shestakov <eithufter@gmail.com>
2024-03-09 19:54:30 +0300

    add CI

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -13,18 +13,18 @@ public class CardDeliveryTest {
 
 
     private String generateDate(int addDays, String pattern){
-        return LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern(pattern));
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
     @Test
     void shouldCardDelivery(){
         open("http://localhost:9999");
-        $("[data-test-id='city']input").setValue("Казань");
-        String planningDate = generateDate(4,"dd.mm.yyyy");
-        $("[data-test-id='date']input").sendKeys(Keys.chord(Keys.SHIFT, Keys.PAGE_UP), Keys.BACK_SPACE);
-        $("[data-test-id='date']input").setValue(planningDate);
-        $("[data-test-id='name']input").setValue("Казаков-Сергеевич Андрей");
-        $("[data-test-id='phone']input").setValue("+79200000000");
-        $("[data-test-id=agreement]").click();
+        $("[data-test-id='city'] input").setValue("Казань");
+        String planningDate = generateDate(5,"dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Казаков Сергеевич Андрей");
+        $("[data-test-id='phone'] input").setValue("+79200000000");
+        $("[data-test-id='agreement']").click();
         $("button.button").click();
         $(".notification__content").shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
